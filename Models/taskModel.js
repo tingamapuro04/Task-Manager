d11bfea53c691b2d3e9e4dfaf20024100fda6d8e
tingamapuro04 <alponcemobutu100@gmail.com>
2023-08-12 11:11:01 +0300

    add schema

---
+++ Models/taskModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const taskSchema = new mongoose.Schema({
+  task: String,
+  completed: Boolean
+})
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
