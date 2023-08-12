7ff26b1bee27ef69991dfe6a08e8d4a7b7c1405a
tingamapuro04 <alponcemobutu100@gmail.com>
2023-08-12 09:22:17 +0300

    Suicide

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const connectDb = require('./database');
 const app = express();
 
 app.get("/", (req, res) => {
   res.status(200).json("We are live and running");
 });
 
-mongoose.connect(process.env.DATABAZE_URL)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("The project is live on port 3000");
-    });
-  })
-  .catch((error) => {
-    throw new Error(error);
-  })
+// mongoose.connect(process.env.DATABAZE_URL)
+//   .then(() => {
+//     app.listen(process.env.PORT, () => {
+//       console.log("The project is live on port 3000");
+//     });
+//   })
+//   .catch((error) => {
+//     throw new Error(error);
+//   })
 
 
